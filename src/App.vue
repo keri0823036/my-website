99aa225450d457b8de27d71526adbf7f031ae8e7
Keri <cindy0823036@gmail.com>
2022-08-29 13:22:52 +0800

    Modify footer, header style and add anchor to the top

--- src/App.vue
+++ src/App.vue
@@ -21,9 +21,16 @@
         v-html="socialMedia.icon"
       />
     </div>
+    <p class="footer__email">cindy0823036@gmail.com</p>
+    <div class="footer__line"></div>
     <p class="footer__uicon-attribute">
       UIcons by <a href="https://www.flaticon.com/uicons">Flaticon</a>
     </p>
+  </div>
+  <div class="go-to-top" @click="scrollToElement('#header')">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="512" height="512">
+      <path d="M21,0H3V2H11.77c-.28,.056-.547,.212-.753,.445l-5.017,5.555h5V24h2V8h5L12.983,2.445c-.206-.233-.473-.389-.753-.445h8.77V0Z"/>
+    </svg>
   </div>
 </template>
 
@@ -38,10 +45,41 @@ export default {
   setup() {
     const { locale } = useI18n()
     const socialMediaData = reactive(socialMedia)
+    function scrollToElement(selector, delay = 0) {
+      const scroll = () => {
+        const $current = document.querySelector(selector)
+
+        if ($current) {
+          const windowHeight = window.innerHeight
+          let $ele = $current
+          let offsetTop = 0
+
+          do {
+            offsetTop += $ele.offsetTop
+          } while (($ele = $ele.offsetParent))
+
+          let offset = offsetTop
+          offset =
+            $current.clientHeight > windowHeight
+              ? offset
+              : offset -
+                (windowHeight - $current.clientHeight) / 2 
+
+          window.scroll({ top: offset, behavior: 'smooth' })
+        } else {
+          console.warn(selector, 'does not exist.')
+        }
+      }
+
+      setTimeout(() => {
+        scroll()
+      }, delay)
+    }
 
     return {
       locale,
-      socialMediaData
+      socialMediaData,
+      scrollToElement
     }
   },
 };
@@ -49,7 +87,6 @@ export default {
 
 <style lang="scss">
 .header {
-  @include position(fixed, $top: 0);
   @include flex($justify-content: space-between, );
   width: calc(100% - 28%);
   padding: 60px 14%;
@@ -88,6 +125,18 @@ export default {
     }
   }
 
+  &__email {
+    @include flex;
+    font-size: 12px;
+    text-align: center;
+  }
+
+  &__line {
+    border-bottom: 1px solid $gray-300;
+    margin: 20px auto;
+    opacity: 0.3;
+  }
+
   &__uicon-attribute {
     color: $gray-300;
     font-size: 12px;
@@ -95,6 +144,29 @@ export default {
     a {
       color: $gray-300;
       text-decoration: underline;
+    }
+  }
+}
+
+.go-to-top {
+  @include flex;
+  @include position(fixed, $bottom: 20px, $right: 20px);
+  background-color: $light-green;
+  width: 40px;
+  height: 40px;
+  padding: 10px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: $dark-green;
+  }
+
+  svg {
+    width: 30px;
+    height: 30px;
+
+    path {
+      fill: $white;
     }
   }
 }
